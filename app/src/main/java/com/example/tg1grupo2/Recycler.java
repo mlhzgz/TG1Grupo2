c8e188decefb29c9b29050553b80aeb170c093b4
Daniel Gimenez <dany@MacBook-Pro-de-Daniel.local>
2022-11-17 18:46:59 +0100

    Recycler

--- app/src/main/java/com/example/tg1grupo2/Recycler.java
+++ app/src/main/java/com/example/tg1grupo2/Recycler.java
@@ -1,8 +1,15 @@
 package com.example.tg1grupo2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.example.tg1grupo2.Metodos.metodos;
+import com.example.tg1grupo2.adapters.Adaptador;
+
+import java.util.ArrayList;
 
 public class Recycler extends AppCompatActivity {
 
@@ -10,5 +17,15 @@ public class Recycler extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
+
+        RecyclerView recycler = findViewById(R.id.recycler);
+        recycler.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recycler.setLayoutManager(layoutManager);
+        ArrayList arraylist = metodos.arrayseries();
+        recycler.setAdapter(new Adaptador(arraylist));
     }
+
+}
 }
